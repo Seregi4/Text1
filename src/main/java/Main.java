629a820e8973b62f8ec0cc00446c3d8cc017cbea
Seregi4 <Seregi4.87@gmail.com>
2021-04-13 19:19:13 +0300

    addLoger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import org.apache.log4j.Logger;
+
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
     public static void main(String[] args) {
+
         String text= """
-                ПРивет пока досвидос. Один, два, три! Создать программу обработки фрагмента текста учебника по программированию с использованием элементов, например: Символ, Слово, Предложение, Текст, Абзац, Листинг, Лексема, Знак препинания и др.
+                ПРивет пока досвидос. Один, два, три, пять!
+                Создать программу обработки фрагмента текста учебника по программированию с использованием элементов, например: Символ, Слово, Предложение, Текст, Абзац, Листинг, Лексема, Знак препинания и др.
                 Приложение должно осуществлять разбор текста на элементы с обязательной возможностью восстановления текста в исходном виде.
                 Можно использовать шаблон Composite.
                 Во всех задачах с формированием текста разрешено заменять табуляции и последовательности пробелов одним пробелом.
@@ -11,11 +16,17 @@ public class Main {
                 """;
 
         BuildTextObjectService service = new BuildTextObjectService();
-
         EditTextService editTextService = new EditTextService();
+
+        System.out.println("Sort by words count");
         System.out.println(editTextService.getSortedSentencesByWordCount(service.parseText(text)));
+
+        System.out.println("Replace words");
         System.out.println(editTextService.getSentencesWithWordReplacements(service.parseText(text)));
+
+        log.error(editTextService);
         System.out.println(service.parseText(text));
+        log.error(service);
     }
 
 }
